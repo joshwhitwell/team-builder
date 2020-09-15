5f450abd064058458d862c15544dc1c1ac00442c
Joshua Whitwell <josh.whitwell@gmail.com>
2020-09-15 15:08:22 -0400

    set up index.css and reset.css

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//Import dependencies
+import React from 'react'
 
-function App() {
+//App component
+export default function App() {
+  
+  //Return App
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Team Member App</h1>
     </div>
-  );
+  )
 }
-
-export default App;
